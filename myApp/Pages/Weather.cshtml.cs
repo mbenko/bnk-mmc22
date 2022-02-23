8f7faaa8851f1ee469544bcb72c71675ec0ac771
Mike Benkovich <mike@benko.com>
2022-02-23 12:06:59 -0600

    Add Weather API call

---
+++ myApp/Pages/Weather.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace myApp.Pages
+{
+    public class WeatherModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
